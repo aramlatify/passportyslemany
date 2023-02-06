88d1b8986b00519c44fb2089822e0288a931510f
aramlatify <58129725+aramlatify@users.noreply.github.com>
2023-02-06 13:37:23 -0800

    Update ButtonTile.js

--- components/table/Button/ButtonTile.js
+++ components/table/Button/ButtonTile.js
@@ -4,7 +4,7 @@ import React from "react";
 const ButtonTile = (handleClick) => (
   <>
   <div>
-  <button type="button" onClick={handleClick} class="btn">
+  <button type="button" onClick={handleClick} className="btn">
     <span className="circle">
       <span className="icon arrow"></span>
     </span>
