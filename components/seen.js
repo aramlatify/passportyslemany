670401129798e7895cc412aaac16a2ec89ca8638
aramlatify <58129725+aramlatify@users.noreply.github.com>
2023-02-07 02:09:04 -0800

    Add files via upload

---
+++ components/seen.js
@@ -0,0 +1,64 @@
+import { useEffect,useRef } from 'react';
+import Views from './view';
+import * as Layout from "./layout";
+import * as Styled from "./seen.style";
+const BlogLayout = ({ slug }) => {
+
+    const effctRan = useRef(false)
+
+    useEffect(() => {
+        
+        if (effctRan.current === false)
+        {
+            const postuser = async () =>{
+
+                const response = await fetch(`/api/views/${slug}`, {
+                    method: 'POST'
+                });
+            }
+            postuser()
+        
+      
+        return ()=>{
+            
+            effctRan.current = true
+        }}
+    }, [slug]);
+
+
+    return (
+        <div>
+        <Styled.MainContainer>
+          <Styled.BlockWrapper>
+      <Styled.Wrapper>
+        <Layout.Row>
+          <Styled.Name>ژمارەی سەردانی کەرانی مەڵپەر</Styled.Name>
+        </Layout.Row>
+        <Layout.Column>
+          <Styled.Stats>
+            <Styled.Stat>
+              
+            <Styled.Label> </Styled.Label>
+            </Styled.Stat>
+            <Styled.Data> <Views slug={slug} /> </Styled.Data>
+            <Styled.Stat>
+            <Styled.Label>جار</Styled.Label>
+             
+             
+            </Styled.Stat>
+            <Styled.Label>بینراوە</Styled.Label>
+           
+            <Styled.Stat>
+            
+              
+            </Styled.Stat>
+          </Styled.Stats>
+        </Layout.Column>
+      </Styled.Wrapper>
+    </Styled.BlockWrapper>
+           
+    </Styled.MainContainer>
+        </div>
+      );
+    };
+    export default BlogLayout;
---
+++ components/view.js
@@ -0,0 +1,16 @@
+import useSWR from 'swr';
+
+const fetcher = async (input) => {
+    const res = await fetch(input);
+    return await res.json();
+};
+const PageViews = ({ slug }) => {
+
+    const { data } = useSWR(`/api/views/${slug}`, fetcher);
+
+   
+    return <>{data?.total ? `${data.total} ` : `–––`}</>;
+
+};
+
+export default PageViews;
