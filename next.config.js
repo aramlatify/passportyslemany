a8ef37598ade434b960d63addab19e5a17d1ad5a
aramlatify <58129725+aramlatify@users.noreply.github.com>
2023-02-06 23:44:12 -0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 module.exports = {
+   eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
   reactStrictMode: true,
   compiler: {
     styledComponents: true,}, webpack: (config) => {
@@ -9,4 +14,5 @@ module.exports = {
   
       return config;
     },
+  
 };
