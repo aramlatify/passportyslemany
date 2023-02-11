d7933eb5d22f67835e708f963376db367a02a77c
aramlatify <58129725+aramlatify@users.noreply.github.com>
2023-02-11 09:25:54 -0800

    Update revalidate.js

--- pages/api/revalidate.js
+++ pages/api/revalidate.js
@@ -1,10 +1,5 @@
 const handler = async (req, res) => {
-  if (req.query.secret !== process.env.NEXT_REVALIDATE_TOKEN) {
-    return res.status(401).json({ message: 'Invalid token' });
-  }
-
-  try {
-    await res.revalidate("/index");
+  await res.revalidate("/");
 
   const pathToRevalidate = `/${
     req.body?.record?.id || req.body?.old_record?.id
@@ -12,11 +7,6 @@ const handler = async (req, res) => {
   await res.revalidate(pathToRevalidate);
 
   return res.send({ revalidated: true });
-  } catch (err) {
-    return res.status(500).send('Error revalidating');
-  }
-  
 };
 
 export default handler;
-
