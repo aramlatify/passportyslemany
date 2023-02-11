ba519fa04741bc184809be82f35d16a281d1557e
aramlatify <58129725+aramlatify@users.noreply.github.com>
2023-02-11 08:35:06 -0800

    Update seen.style.js

--- components/seen.style.js
+++ components/seen.style.js
@@ -11,45 +11,22 @@ const colors = {
   };
   
 
-export const TwitchIcon = styled.img.attrs({
-  src: "/images/twitch.svg",
-})`
-  max-width: 20px;
-  fill: #fff;
-  transition: all 0.3s;
-
-  opacity: ${(props) => (props.url ? 0.75 : 0.1)};
-  cursor: ${(props) => (props.url ? "pointer" : "not-allowed")};
-
-  &:hover {
-    transform: ${(props) => (props.url ? "scale(1.15)" : 0)};
-    opacity: ${(props) => (props.url ? 1 : 0.1)};
-  }
-`;
-
-export const ContactIcons = styled.div`
-  display: flex;
-  height: 24px;
-  position: absolute;
-  right: 24px;
-  top: -6px;
-`;
-
 export const Name = styled.h3`
   margin-bottom: 2rem;
-  margin-left: 48px;
+  flex-grow: 1;
+  text-align: center;
   white-space: nowrap;
   font-weight: 1000;
-  font-size: 2.5rem;
-  text-align: right;
+  font-size: 1.5rem;
+  
   line-height: 1.5em;
   color: ${colors.light0};
 `;
 
 export const League = styled.span`
-  margin-left: 12px;
+  margin-left: 0px;
   font-family: "ApexBold";
-  padding-top: 4px;
+  
   color: ${colors.accent2};
 `;
 
@@ -63,7 +40,7 @@ export const Stat = styled.span`
 export const Stats = styled.div`
   display: flex;
   max-width: 625px;
-  padding: 8px 12px;
+ 
   transition: all 0.3s;
 
   background: rgba(255, 255, 255, 0);
@@ -88,22 +65,22 @@ export const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
  
-  
+  border-radius: 38px;
   border-left: 2px solid ${colors.accent0};
-  padding: 0px 12px 0px 14px;
+  border-right: 2px solid ${colors.accent0};
   font-family: "Dank Mono";
 `;
 export const BlockWrapper = styled.div`
+
+border-radius: 38px;
   
-  padding: 18px 0 12px;
-  margin-bottom: 24px;
   background: rgba(255, 255, 255, 0.05);
 `;
 
 export const MainContainer = styled.div`
   display: flex;
   flex-direction: row;
-  padding: 24px 48px;
+ 
   margin: 0 auto;
 
   position: relative;
@@ -112,56 +89,4 @@ export const MainContainer = styled.div`
   ${BlockWrapper}:nth-of-type() {
     background: rgba(255, 255, 255, 0.05);
   }
-`;
-const bgColor = "orange";
-
-export const Message = styled.div`
-  padding: 1rem 1.5rem;
-  border-radius: 25px;
-  color: ${colors.light0};
-  position: relative;
-  width: 50%;
-  margin: 0 auto;
-  text-align: left;
-  display: flex;
-  align-items: center;
-
-  font-weight: 1000;
-  font-size: 1.5rem;
-  text-align: right;
-  line-height: 1.5em;
-  background-color: ${colors.dark1};
-
-  &::after,
-  &::before {
-    content: "";
-    position: absolute;
-    width: 23px;
-    height: 16px;
-  }
-
-  &::before {
-    bottom: 3px;
-    right: -22px;
-    background-color: ${colors.dark0};
-    width: 24px;
-    height: 22px;
-    z-index: 2;
-    border-bottom-left-radius: 15px;
-    transform: rotate(25deg);
-  }
-
-  &::after {
-    right: -16px;
-    bottom: 0;
-    border-bottom-left-radius: 15px;
-    background-color: ${bgColor};
-    border: 2px solid ${bgColor};
-    z-index: 1;
-  }
-`;
-
-export const Img = styled.img`
-  width: 500px;
-  height: 50px;
 `;
