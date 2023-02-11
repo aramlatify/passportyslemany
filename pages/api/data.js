7adf92f03d8c6eb30cfe0bccd237352549cf22ad
aramlatify <58129725+aramlatify@users.noreply.github.com>
2023-02-11 08:36:56 -0800

    Add files via upload

---
+++ pages/api/data.js
@@ -0,0 +1,348 @@
+const B = [
+    {
+        "Fullname": "أحمد انور مجيد",
+        "Shurename": "مجيد",
+        "Mothername": "فيان صالح",
+        "Location": "پاسپۆرتی کەلار",
+        "id": 2
+    },
+    {
+        "Fullname": "أحمد هردي كاظم",
+        "Shurename": "الزنكنه",
+        "Mothername": "بزوين يحيى",
+        "Location": "پاسپۆرتی کەلار",
+        "id": 3
+    },
+    {
+        "Fullname": "آرام جالاك خورشيد",
+        "Shurename": "الزنكنه",
+        "Mothername": "سعديه طالب",
+        "Location": "پاسپۆرتی کەلار",
+        "id": 4
+    },
+    {
+        "Fullname": "آرياس حمه جان عزيز",
+        "Shurename": "ترخاني",
+        "Mothername": "حلاو حمه علي",
+        "Location": "پاسپۆرتی کەلار",
+        "id": 5
+    },
+    {
+        "Fullname": "آرينا محمد جاسم",
+        "Shurename": "زركوش",
+        "Mothername": "هند ستار",
+        "Location": "پاسپۆرتی کەلار",
+        "id": 6
+    },
+    {
+        "Fullname": "آكرم احمد حاجي عبدالرحمان",
+        "Shurename": "عبدالرحمان",
+        "Mothername": "امينه محمود",
+        "Location": "پاسپۆرتی کەلار",
+        "id": 7
+    },
+    {
+        "Fullname": "آلاء محمود حسن",
+        "Shurename": "حسن",
+        "Mothername": "زارا علي",
+        "Location": "پاسپۆرتی کەلار",
+        "id": 8
+    },
+    {
+        "Fullname": "آمنه سعيد محمد صالح",
+        "Shurename": "الجاف",
+        "Mothername": "فتحيه احمد",
+        "Location": "پاسپۆرتی کەلار",
+        "id": 9
+    },
+    {
+        "Fullname": "آنس آسو سعدالله",
+        "Shurename": "الجمور",
+        "Mothername": "حسيبه احمد",
+        "Location": "پاسپۆرتی کەلار",
+        "id": 10
+    },
+    {
+        "Fullname": "آواز محمود عبدالكريم",
+        "Shurename": "عبدالكريم",
+        "Mothername": "نازه نين احمد",
+        "Location": "پاسپۆرتی کەلار",
+        "id": 11
+    },
+    {
+        "Fullname": "آواز فتاح محمد صالح",
+        "Shurename": "محمد صالح",
+        "Mothername": "سعدية محمد",
+        "Location": "پاسپۆرتی کەلار",
+        "id": 12
+    },
+    {
+        "Fullname": "آواز محمد حسين",
+        "Shurename": "حسين",
+        "Mothername": "سعدي عبدالقادر",
+        "Location": "پاسپۆرتی کەلار",
+        "id": 13
+    },
+    {
+        "Fullname": "أحمد انور مجيد",
+        "Shurename": "مجيد",
+        "Mothername": "فيان صالح",
+        "Location": "پاسپۆرتی کەلار",
+        "id": 14
+    },
+    {
+        "Fullname": "أحمد هردي كاظم",
+        "Shurename": "الزنكنه",
+        "Mothername": "بزوين يحيى",
+        "Location": "پاسپۆرتی کەلار",
+        "id": 15
+    },
+    {
+        "Fullname": "أحمد حسام على",
+        "Shurename": "الزندي",
+        "Mothername": "نسرين محمد",
+        "Location": "پاسپۆرتی کەلار",
+        "id": 16
+    },
+    {
+        "Fullname": "أحمد عدنان روضان",
+        "Shurename": "روضان",
+        "Mothername": "ثريا ابراهيم",
+        "Location": "پاسپۆرتی کەلار",
+        "id": 17
+    },
+    {
+        "Fullname": "أحمد حسين مجيد",
+        "Shurename": "الجاف",
+        "Mothername": "زهره رسول",
+        "Location": "پاسپۆرتی کەلار",
+        "id": 18
+    },
+    {
+        "Fullname": "أحمد حسن حسين",
+        "Shurename": "حسين",
+        "Mothername": "اشواق ابراهيم",
+        "Location": "پاسپۆرتی کەلار",
+        "id": 19
+    },
+    {
+        "Fullname": "أدريس محمد مولود",
+        "Shurename": "الباجلان",
+        "Mothername": "حلاو عارف",
+        "Location": "پاسپۆرتی کەلار",
+        "id": 20
+    },
+    {
+        "Fullname": "أرام ابراهيم حميد",
+        "Shurename": "الداودي",
+        "Mothername": "نوريه شامار",
+        "Location": "پاسپۆرتی کەلار",
+        "id": 21
+    },
+    {
+        "Fullname": "أرشد فائق عبدالله",
+        "Shurename": "عبدالله",
+        "Mothername": "صبرى عبدالله",
+        "Location": "پاسپۆرتی کەلار",
+        "id": 22
+    },
+    {
+        "Fullname": "أسراء كوران مراد",
+        "Shurename": "مراد",
+        "Mothername": "شاديه رشيد",
+        "Location": "پاسپۆرتی کەلار",
+        "id": 23
+    },
+    {
+        "Fullname": "أفين عبدالجبار صابر",
+        "Shurename": "الدلوي",
+        "Mothername": "نوريه عبدالرحمان",
+        "Location": "پاسپۆرتی کەلار",
+        "id": 24
+    },
+    {
+        "Fullname": "ألند اركان محمد",
+        "Shurename": "محمد",
+        "Mothername": "مروه حمزه",
+        "Location": "پاسپۆرتی کەلار",
+        "id": 25
+    },
+    {
+        "Fullname": "ألياس محمد عزيز",
+        "Shurename": "الجاف",
+        "Mothername": "شادان عزيز",
+        "Location": "پاسپۆرتی کەلار",
+        "id": 26
+    },
+    {
+        "Fullname": "ألين ديار جمال",
+        "Shurename": "زركوش",
+        "Mothername": "فيان اسماعيل",
+        "Location": "پاسپۆرتی کەلار",
+        "id": 27
+    },
+    {
+        "Fullname": "أمجد محمود حاجي احمد",
+        "Shurename": "حاجي احمد",
+        "Mothername": "عطيه صالح",
+        "Location": "پاسپۆرتی کەلار",
+        "id": 28
+    },
+    {
+        "Fullname": "أمير محمدزاهد محمد علي",
+        "Shurename": "محمد علي",
+        "Mothername": "بهيه احمد",
+        "Location": "پاسپۆرتی کەلار",
+        "id": 29
+    },
+    {
+        "Fullname": "أمير سالار عبدالقادر",
+        "Shurename": "عبدالقادر",
+        "Mothername": "آمينه توفيق",
+        "Location": "پاسپۆرتی کەلار",
+        "id": 30
+    },
+    {
+        "Fullname": "أيمن اركان علي",
+        "Shurename": "شاترى",
+        "Mothername": "روشنا حميد",
+        "Location": "پاسپۆرتی کەلار",
+        "id": 31
+    },
+    {
+        "Fullname": "أيمن طاهر احمد",
+        "Shurename": "البيواني",
+        "Mothername": "ليلى احمد",
+        "Location": "پاسپۆرتی کەلار",
+        "id": 32
+    },
+    {
+        "Fullname": "أيمن فريا صباح",
+        "Shurename": "جاف",
+        "Mothername": "ساره يوسف",
+        "Location": "پاسپۆرتی کەلار",
+        "id": 33
+    },
+    {
+        "Fullname": "أيمن قادر صابر",
+        "Shurename": "البياتي",
+        "Mothername": "آيفان ابراهيم",
+        "Location": "پاسپۆرتی کەلار",
+        "id": 34
+    },
+    {
+        "Fullname": "أيوب مجيد محمود",
+        "Shurename": "الجاف",
+        "Mothername": "أمينه محمد",
+        "Location": "پاسپۆرتی کەلار",
+        "id": 35
+    },
+    {
+        "Fullname": "أيوب حميد كريم",
+        "Shurename": "نيرنجي",
+        "Mothername": "شكريه عباس",
+        "Location": "پاسپۆرتی کەلار",
+        "id": 36
+    },
+    {
+        "Fullname": "أيوب عمر رشيد",
+        "Shurename": "الشاطرى",
+        "Mothername": "حياة عبدالقادر",
+        "Location": "پاسپۆرتی کەلار",
+        "id": 37
+    },
+    {
+        "Fullname": "أيوب ديار جمال",
+        "Shurename": "زركوش",
+        "Mothername": "فيان اسماعيل",
+        "Location": "پاسپۆرتی کەلار",
+        "id": 38
+    },
+    {
+        "Fullname": "إيلا خالد عبدالله",
+        "Shurename": "المندلاوي",
+        "Mothername": "شرمين عباس",
+        "Location": "پاسپۆرتی کەلار",
+        "id": 39
+    },
+    {
+        "Fullname": "ئابو علي رحيم",
+        "Shurename": "رحيم",
+        "Mothername": "برزين صالح",
+        "Location": "پاسپۆرتی کەلار",
+        "id": 40
+    },
+    {
+        "Fullname": "ئاتيلا كاوه احمد",
+        "Shurename": "شاطري",
+        "Mothername": "زوان نجم الدين",
+        "Location": "پاسپۆرتی کەلار",
+        "id": 41
+    },
+    {
+        "Fullname": "ئارام صالح محمد",
+        "Shurename": "محمد",
+        "Mothername": "أشتي كريم",
+        "Location": "پاسپۆرتی کەلار",
+        "id": 42
+    },
+    {
+        "Fullname": "ئارو ضياء صلاح",
+        "Shurename": "الداودي",
+        "Mothername": "تارا عبدالله",
+        "Location": "پاسپۆرتی کەلار",
+        "id": 43
+    },
+    {
+        "Fullname": "ئاري كمال نامق",
+        "Shurename": "نامق",
+        "Mothername": "بروين عزيز",
+        "Location": "پاسپۆرتی کەلار",
+        "id": 44
+    },
+    {
+        "Fullname": "ئارياس سركوت حمه امين",
+        "Shurename": "حمه امين",
+        "Mothername": "زيان رفيق",
+        "Location": "پاسپۆرتی کەلار",
+        "id": 45
+    },
+    {
+        "Fullname": "ئارين جاسم محمد امين",
+        "Shurename": "الجاف",
+        "Mothername": "بخشان عزالدين",
+        "Location": "پاسپۆرتی کەلار",
+        "id": 46
+    },
+    {
+        "Fullname": "ئاشنا حمزه خليل",
+        "Shurename": "داودي",
+        "Mothername": "عائشه حسن",
+        "Location": "پاسپۆرتی کەلار",
+        "id": 47
+    },
+    {
+        "Fullname": "ئافار أميد احمد",
+        "Shurename": "احمد",
+        "Mothername": "سهيله قوربانى",
+        "Location": "پاسپۆرتی کەلار",
+        "id": 48
+    },
+    {
+        "Fullname": "ئافيستا عبدالله محمد",
+        "Shurename": "الشاطري",
+        "Mothername": "نازيب حمه ظاهر",
+        "Location": "پاسپۆرتی کەلار",
+        "id": 49
+    },
+    {
+        "Fullname": "ئاكا كوردستان حميد",
+        "Shurename": "روغزاي",
+        "Mothername": "فريال عمر",
+        "Location": "پاسپۆرتی کەلار",
+        "id": 50
+    }
+  ];
+
+  
+  export default B;
---
+++ pages/api/revalidate.js
@@ -0,0 +1,22 @@
+const handler = async (req, res) => {
+  if (req.query.secret !== process.env.NEXT_REVALIDATE_TOKEN) {
+    return res.status(401).json({ message: 'Invalid token' });
+  }
+
+  try {
+    await res.revalidate("/index.js");
+
+  const pathToRevalidate = `/${
+    req.body?.record?.id || req.body?.old_record?.id
+  }`;
+  await res.revalidate(pathToRevalidate);
+
+  return res.send({ revalidated: true });
+  } catch (err) {
+    return res.status(500).send('Error revalidating');
+  }
+  
+};
+
+export default handler;
+
